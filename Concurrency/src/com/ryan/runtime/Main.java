42819b75215feceb305ace6ec158b15401830c0e
rbartolay <rbartolay@192.168.4.78>
2017-11-25 10:10:30 +0800

    concurrency

---
+++ Concurrency/src/com/ryan/runtime/Main.java
@@ -0,0 +1,19 @@
+package com.ryan.runtime;
+
+import java.util.concurrent.Callable;
+
+public class Main {
+	public static void main(String[] args) {
+
+	}
+}
+
+class Test implements Callable<String> {
+
+	@Override
+	public String call() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
